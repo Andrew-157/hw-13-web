b560a66f8f6163f515f63e050f5e857c89618459
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-21 13:25:37 +0200

    some primitive views

--- app/finances/urls.py
+++ app/finances/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'finances'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('income/', views.income, name='income'),
+    path('outcome/', views.outcome, name='outcome'),
+    path('statistics/', views.statistics, name='statistics'),
+    path('add_income/', views.add_income, name='add_income')
 ]
--- app/finances/views.py
+++ app/finances/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 
 def index(request):
-    return HttpResponse("Welcome to Financial Diary")
+    return render(request, 'finances/index.html')
+
+
+def income(request):
+    return render(request, 'finances/income.html')
+
+
+def outcome(request):
+    return HttpResponse("This is outcome page")
+
+
+def statistics(request):
+    return HttpResponse("This is statistics page")
+
+
+def add_income(request):
+    print(request.POST['income'])
+    return HttpResponseRedirect(reverse('finances:index'))
